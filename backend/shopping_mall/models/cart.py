832989a6249d838a2ec3031d03451ae7b71e0c85
Yehwan Baek <baekokok98@gmail.com>
2024-03-07 17:52:59 +1100

    cart model in progress2

--- backend/shopping_mall/models/cart.py
+++ backend/shopping_mall/models/cart.py
@@ -12,4 +12,33 @@ class ShoppingCart:
         self.items = []
 
     def add_item(self, product_id, quantity):
-        
+        existing_item = next((item for item in self.items if item.product_id == product_id), None)
+
+        if existing_item:
+            existing_item.quantity += quantity
+        else:
+            new_item = CartItem(product_id=product_id, quantity=quantity)
+            self.items.append(new_item)
+
+    def remove_item(self, product_id):
+        self.items = [item for item in self.items if item.product_id != product_id]
+
+    def update_quantity(self, product_id, quantity):
+        item = next((item for item in self.items if item.product_id == product_id), None)
+
+        if item:
+            item.quantity = quantity
+
+    def clear_cart(self):
+        self.items = []
+
+    def calculate_total(self):
+        products_collection = current_app.mongo.shopping_mall.products
+        total = 0
+
+        for item in self.items:
+            product = products_collection.find_one({'_id': ObjectId(item.product_id)})
+            if product:
+                total += product['price'] * item.quantity
+
+        return total
